70b9c49bc9d5a9a74324d4c5ca2ee14370cfe101
Stephen Mathieson <smath23@gmail.com>
2013-06-10 14:42:34 -0400

    event normalization for ie8

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+/**
+ * Normalize an `event`
+ *
+ * @param {Event} [e]
+ * @return {Event}
+ * @api private
+ */
+
+function normalize(e) {
+  e = e || window.event;
+
+  // layers
+  if (e.layerX) {
+    e.offsetX = e.layerX;
+    e.offsetY = e.layerY;
+  }
+
+  // set relatedTarget for mouse events
+  if (!e.relatedTarget) {
+    if (e.type === 'mouseover') {
+      e.relatedTarget = e.fromElement;
+    } else if (e.type === 'mouseout') {
+      e.relatedTarget = e.toElement;
+    }
+  }
+
+  // KeyboardEvent#which
+  e.which = e.keyCode || e.charCode;
+
+  // Event#target
+  e.target = e.target || e.srcElement;
+
+  // preventDefault and stopPropagation
+  if (typeof e.preventDefault !== 'function') {
+    e.preventDefault = function () {
+      this.returnValue = false;
+    };
+    e.stopPropagation = function () {
+      this.cancelBubble = true;
+    };
+  }
+
+  return e;
+}
 
 /**
  * Bind `el` event `type` to `fn`.
@@ -13,10 +57,15 @@
 exports.bind = function(el, type, fn, capture){
   if (el.addEventListener) {
     el.addEventListener(type, fn, capture || false);
-  } else {
-    el.attachEvent('on' + type, fn);
+    return fn;
   }
-  return fn;
+
+  fn._normalized = function (e) {
+    fn.call(el, normalize(e));
+  };
+
+  el.attachEvent('on' + type, fn._normalized);
+  return fn._normalized;
 };
 
 /**
@@ -34,7 +83,7 @@ exports.unbind = function(el, type, fn, capture){
   if (el.removeEventListener) {
     el.removeEventListener(type, fn, capture || false);
   } else {
-    el.detachEvent('on' + type, fn);
+    el.detachEvent('on' + type, fn._normalized);
   }
   return fn;
 };
